a30b2da21a05d3d9612b86f960c36a0b7d1fa80e
Muhammad Talha (Mac) <talhahilside@gmail.com>
2019-01-13 20:15:21 -0800

    init mltk

--- postagger_model.h
+++ postagger_model.h
@@ -2,43 +2,11 @@
 #define POSTAGGER_H
 
 #include <string>
-#include "maxent/maxent.h"
 
 using namespace std;
 
 class Postagger_Model {
-    ME_Model postagger_model;
-    bool postagger_model_loaded;
 
-    string train_file, test_file;
-
-    public:
-        Postagger_Model(string train_file, string test_file):
-            postagger_model_loaded(false), train_file(train_file), test_file(test_file) {}
-
-        struct Token {
-            string str;
-            string pos;
-            Token(const string & str, const string & pos): str(str), pos(pos) {}
-        };
-
-        void test();
-        void train();
-        vector<Token> classify_line(const string & str);
-
-        ME_Model get_postagger_model(){
-            if (!postagger_model_loaded){
-                if (!postagger_model.load_from_file("postagger.model")){
-                    train();
-                }
-            }
-
-            return postagger_model;
-        }
-
-    private:
-        ME_Sample sample(const vector<Token> & vt, int i);
-        vector<Token> read_line(const string & line);
 };
 
 #endif
