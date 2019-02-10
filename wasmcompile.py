2112fbec65ba99fbf1aaa83dc0ba2d75fc0a1059
Muhammad Talha (Mac) <talhahilside@gmail.com>
2019-02-10 12:23:48 -0800

    fix up compile issues

--- wasmcompile.py
+++ wasmcompile.py
@@ -1,8 +1,8 @@
   #!/usr/bin/python
 
 wasm_js_file = "extractor.js"
-keyword_extractor_js_file = "keyword_extractor.js"
-main_js = "content_script.js"
+background_script_js_file = "keyword_extractor.js"
+main_js = "compiled_background_script.js"
 
 def main():
   # correctly read extractor.wasm and extractor.data
@@ -11,12 +11,11 @@ def main():
         .replace('scriptDirectory+path', 'chrome.runtime.getURL(path)')\
         .replace('var REMOTE_PACKAGE_BASE="extractor.data";', 'var REMOTE_PACKAGE_BASE=chrome.runtime.getURL(PACKAGE_NAME);')
 
-  with open(keyword_extractor_js_file
-) as f:
-    keyword_js_text = f.read()
+  with open(background_script_js_file) as f:
+    background_script_js_text = f.read()
 
   with open(main_js, "w") as f:
-    f.write(wasm_js_text + "\n" + keyword_js_text)
+    f.write(wasm_js_text + "\n" + background_script_js_text)
 
 if __name__ == "__main__":
     main()
